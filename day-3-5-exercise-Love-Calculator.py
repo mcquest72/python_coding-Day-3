e7ed6a86d866a6df8771402eb8741ca51b61731f
McQuest-72 <mcquest2347@gmail.com>
2023-12-13 16:41:57 -0800

    completed coding love calculator

--- day-3-5-exercise-Love-Calculator.py
+++ day-3-5-exercise-Love-Calculator.py
@@ -11,3 +11,31 @@ name2 = input('What is their Name? \n')
 combine = name1 + name2
 lowerCase = combine.lower()
 
+# Counting True love letters in user names
+t = lowerCase.count('t')
+r = lowerCase.count('r')
+u = lowerCase.count('u')
+e = lowerCase.count('e')
+
+true = t + r + u + e
+
+l = lowerCase.count('l')
+o = lowerCase.count('o')
+v = lowerCase.count('v')
+e = lowerCase.count('e')
+
+love = l + o + v + e
+
+# getting the love score from user input
+love_score = int(str(true) + str(love))
+
+# using conditional statements to determine output
+if (love_score < 10) or (love_score > 90):
+    print(f'Your score is {love_score}, you go together like coke and mentos')
+
+elif love_score >= 40 and love_score <= 50:
+    print(f'Your score is {love_score}, you are alright together')
+
+else:
+    print(f'Your score is {love_score}')
+
