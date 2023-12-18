169d27281fad4a6c54c960765891397853e9f697
McQuest-72 <mcquest2347@gmail.com>
2023-12-17 20:14:40 -0800

    conditional statement added to Treasure Island Project

--- Treasure Island Project.py
+++ Treasure Island Project.py
@@ -2,3 +2,20 @@
 The essence of this game is for the player to find
 Treasures.
 """
+print()
+print('Welcome to Treasure Island, your mission is to find the treasure')
+
+# User input is needed
+choice1 = input('You\ are at a crossroad. Where do you want to go? Type "Left" or "Right" \n').lower()
+
+# using conditional statement
+if choice1 == 'left':
+
+    choice2 = input('Your\ have come to a lake. There is an island in the middle of the lake. Type "wait" to wait for a boat. Type "swim" to swim across.').lower()
+
+    if choice2 == 'wait':
+
+        choice3 = 
+
+elif choice == 'Left':
+    print('do you want to swim or wait?')
